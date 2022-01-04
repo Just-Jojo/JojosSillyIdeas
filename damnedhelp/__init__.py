9f105f6dab76b935f37eae71a0f524a9bc395d9d
Just-Jojo <justjojogh@gmail.com>
2022-01-04 02:02:23 -0500

    [DamnedHelp] Initial commit

---
+++ damnedhelp/__init__.py
@@ -0,0 +1,20 @@
+from redbot.core import commands
+import discord
+
+
+class DamnedHelp(commands.Cog):
+    @commands.Cog.listener()
+    async def on_message_without_command(self, message: discord.Message) -> None:
+        if not message.guild or message.author.bot or message.guild.id != 133049272517001216:
+            return
+        elif message.content != "!help":
+            return
+        await ctx.send(
+            "There is no help for the damned.",
+            reference=message.to_reference(fail_if_not_exists=False),
+            mention_author=False
+        )
+
+
+def setup(bot):
+    bot.add_cog(DamnedHelp())
