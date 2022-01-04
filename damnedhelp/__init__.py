8b6a7bd2e979d85b23f9b99ca575ea6ea75abf6e
Just-Jojo <justjojogh@gmail.com>
2022-01-04 02:09:24 -0500

    [DamnedHelp] Hmm

--- damnedhelp/__init__.py
+++ damnedhelp/__init__.py
@@ -1,17 +1,24 @@
 from redbot.core import commands
 import discord
 
+import logging
+
+log = logging.getLogger("red.sillyideas.damnedhelp")
 
 class DamnedHelp(commands.Cog):
     @commands.Cog.listener()
     async def on_message_without_command(self, message: discord.Message) -> None:
         if not message.guild:
+            log.info("no guild")
             return
         elif message.author.bot:
+            log.info("author was bot")
             return
         elif message.guild.id != 133049272517001216:
+            log.info("not red")
             return
         elif message.content != "!help":
+            log.info("not !help")
             return
         await ctx.send(
             "There is no help for the damned.",
