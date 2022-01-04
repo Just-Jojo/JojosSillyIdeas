e36473f0b8672e87e1d9950a476f7bdf48c75245
Just-Jojo <justjojogh@gmail.com>
2022-01-04 02:15:59 -0500

    [DamnedHelp] sleep and remove logging calls

--- damnedhelp/__init__.py
+++ damnedhelp/__init__.py
@@ -2,6 +2,7 @@ from redbot.core import commands
 import discord
 
 import logging
+import asyncio
 
 log = logging.getLogger("red.sillyideas.damnedhelp")
 
@@ -9,17 +10,14 @@ class DamnedHelp(commands.Cog):
     @commands.Cog.listener()
     async def on_message_without_command(self, message: discord.Message) -> None:
         if not message.guild:
-            log.info(f"no guild {message.author = }")
             return
         elif message.author.bot:
-            log.info(f"author was bot {message.author = }")
             return
         elif message.guild.id != 133049272517001216:
-            log.info(f"not red {message.author = }")
             return
         elif message.content != "!help":
-            log.info(f"not !help {message.author = }")
             return
+        await asyncio.sleep(5)
         await message.channel.send(
             "There is no help for the damned.",
             reference=message.to_reference(fail_if_not_exists=False),
