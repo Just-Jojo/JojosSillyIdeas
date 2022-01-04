6855f34d5ffb81e2b743c35a7d9e803784bbfd92
Just-Jojo <justjojogh@gmail.com>
2022-01-04 02:05:18 -0500

    Hm

--- damnedhelp/__init__.py
+++ damnedhelp/__init__.py
@@ -5,7 +5,11 @@ import discord
 class DamnedHelp(commands.Cog):
     @commands.Cog.listener()
     async def on_message_without_command(self, message: discord.Message) -> None:
-        if not message.guild or message.author.bot or message.guild.id != 133049272517001216:
+        if not message.guild:
+            return
+        elif message.author.bot:
+            return
+        elif message.guild.id != 133049272517001216:
             return
         elif message.content != "!help":
             return
