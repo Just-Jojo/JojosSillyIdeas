1239487f9987e5bb112b3531f857aca8928a8d3c
Just-Jojo <justjojogh@gmail.com>
2022-01-06 03:26:07 -0500

    [Userinfo] V1.0.1 What on god's green and brown earth have I created

--- userinfo/__init__.py
+++ userinfo/__init__.py
@@ -1,4 +1,4 @@
-from redbot.core import commands
+from redbot.core import commands, Config
 from redbot.core.bot import Red
 
 from typing import Optional
@@ -6,35 +6,59 @@ import discord
 
 __red_end_user_data_statement__ = "This cog does not store end user data."
 
+@commands.command(name="userinfo")
+@commands.guild_only()
+async def userinfo(self, ctx: commands.Context, *, user: discord.Member = None):
+    """Check the info for a user
+
+    **Arguments**
+        - `user` The user you want to look at. Defaults to yourself.
+    """
+    that, plural = ("That user", "s") if user else ("You", "")
+    await ctx.send(f"{that} probably exist{plural}.")
+
 
 class Userinfo(commands.Cog):
     def __init__(self, bot: Red) -> None:
         self.bot = bot
+        self.config = Config.get_conf(self, 544974305445019651, True)
+        self.config.register_global(toggled=False)
         self._command: Optional[commands.Command] = None
+        self._toggled: bool = False
+        userinfo.cog = self
 
-    __version__ = "1.0.0"
+    __version__ = "1.0.1"
 
     def cog_unload(self) -> None:
         if self._command:
             self.bot.remove_command("userinfo")
             self.bot.add_command(self._command)
 
+    def _inject_eject_cmd(self, inject: bool) -> None:
+        cmd = userinfo if inject else self._command
+        self.bot.remove_command("userinfo")
+        if not cmd:
+            return
+        self.bot.add_command(cmd)
+
+    @commands.command()
+    @commands.is_owner()
+    async def userinfotoggle(self, ctx: commands.Command):
+        """Toggle the userinfo command"""
+        coro = self.config.toggled
+        toggle = not await coro()
+        await coro.set(toggle)
+        d = "Enabled" if toggle else "Disabled"
+        await ctx.send(f"{d} the overwritten userinfo command.")
+        self._inject_eject_cmd(toggle)
+
     @classmethod
     async def init(cls, bot: Red) -> "Userinfo":
         self = cls(bot)
-        self._command = bot.remove_command("userinfo")
+        self._toggled = await self.config.toggled()
+        if self._toggled:
+            self._inject_eject_cmd(True)
         return self
-
-    @commands.command(name="userinfo")
-    @commands.guild_only()
-    async def userinfo(self, ctx: commands.Context, user: discord.Member = None):
-        """Check the info for a user
-
-        **Arguments**
-            - `user` The user you want to look at. Defaults to yourself.
-        """
-        that, plural = ("That user", "s") if user else ("You", "")
-        await ctx.send(f"{that} probably exist{plural}.")
 
 
 async def setup(bot: Red) -> None:
