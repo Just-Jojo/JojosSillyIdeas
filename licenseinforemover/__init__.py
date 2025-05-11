941b83e248cb9e45de236048941a2f9c2972256b
Just-Jojo <62262765+Just-Jojo@users.noreply.github.com>
2025-05-10 21:29:01 -0400

    [LicenseInfoRemover] Change a few things

--- licenseinforemover/__init__.py
+++ licenseinforemover/__init__.py
@@ -1,18 +1,22 @@
 from redbot.core import commands, Config
 from redbot.core.bot import Red
 
+from typing import Optional
+
 
 class LicenseInfoRemover(commands.Cog):
     """Remove the licenseinfo command honestly"""
 
     @commands.command()
     @commands.is_owner()
-    async def licenseinforemove(self, ctx: commands.Context, please: str = None):
+    async def licenseinforemove(self, ctx: commands.Context, please: Optional[str] = None):
         """Remove the license info command"""
         if please is None:
-            return await ctx.send("Say please maybe?")
+            await ctx.send("Say please, maybe?")
+            return
         elif please.lower() == "please?":
-            return await ctx.send("Nah mate, you need a license to run Red, innit?")
+            await ctx.send("Nah man, you need a license to run Red. That's kinda fucked up, dude...")
+            return
         await ctx.send("That wasn't a pleaaaaase!")
 
 
